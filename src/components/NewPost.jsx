0630fa37c2d660f87a68b0f30e8e1f0b7deb71b4
Ashok <ashokkumarsingh@gmail.com>
2024-03-18 20:49:54 +0530

    Saving code associated with Section 2 Lesson 25. Working with State 18 March 2024

---
+++ src/components/NewPost.jsx
@@ -0,0 +1,26 @@
+import {useState} from 'react'; // react hook
+import classes from './NewPost.module.css';
+
+function NewPost() {
+    const [enteredBody, setEnteredBody] = useState('');
+
+    function changeBodyHandler(event){
+    setEnteredBody(event.target.value);
+    }
+
+  return (
+    <form className={classes.form}>
+      <p>
+        <label htmlFor="body">Text</label>
+        <textarea id="body" required rows={3} onChange={changeBodyHandler}/>
+      </p>
+      <p>{enteredBody}</p>
+      <p>
+        <label htmlFor="name">Your name</label>
+        <input type="text" id="name" required />
+      </p>
+    </form>
+  );
+}
+
+export default NewPost;
--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,12 +1,17 @@
 import Post from './Post';
-import classes from './PostsList.module.css'
+import NewPost from './NewPost';
+import classes from './PostsList.module.css';
 
 function PostsList () {
     return (
+        <>
+        <NewPost/>
         <ul className={classes.posts}>
-                <Post author="Ashok Singh" body="Ashok's post"/>
-                <Post author="Asheesh Mathur" body="Asheesh's post"/>
-        </ul>
+            <Post author="Ashok Singh" body="Ashok's post"/>
+            <Post author="Asheesh Mathur" body="Asheesh's post"/>
+        </ul>   
+        </>
+
     );
 }
 
