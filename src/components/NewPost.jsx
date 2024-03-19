1ca1cbaed628c5519b9b0491b7ca5491fe8ed2fe
Ashok <ashokkumarsingh@gmail.com>
2024-03-19 11:18:33 +0530

    Saving code for Section 2 Lesson 25. Working with State. 19 March 2024

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,9 +1,14 @@
 import {useState} from 'react'; // react hook
 import classes from './NewPost.module.css';
 
+// Note: The hook has to be executed within the component as shown below.
+// This registers a new state value that belongs to this component.
+// The useState function returns an array of exactly two elements.
+// The first one is the current state value which is an empty string useState('') 
+// and the second one is a state updating function.
+// This will cause react to run the NewPost() function on every "change".
 function NewPost() {
-    const [enteredBody, setEnteredBody] = useState('');
-
+    const [enteredBody, setEnteredBody] = useState(''); 
     function changeBodyHandler(event){
     setEnteredBody(event.target.value);
     }
@@ -14,7 +19,7 @@ function NewPost() {
         <label htmlFor="body">Text</label>
         <textarea id="body" required rows={3} onChange={changeBodyHandler}/>
       </p>
-      <p>{enteredBody}</p>
+      <p id="textShown">{enteredBody}</p>
       <p>
         <label htmlFor="name">Your name</label>
         <input type="text" id="name" required />
